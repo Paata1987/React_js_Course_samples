96c4301c8906a983210465e5c23ada58d3a2c151
Paata Home <paata.tepnadze@gmail.com>
2022-12-20 10:19:26 +0100

    create increement/decrement class component  OLD methids

--- react_sam/src/App.js
+++ react_sam/src/App.js
@@ -1,13 +1,10 @@
+import ClassCounter from './components/ClassCounter';
 import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
-
-      <Counter />
-      <Counter />
-      <Counter />
+      <ClassCounter />
     </div>
   );
 }
