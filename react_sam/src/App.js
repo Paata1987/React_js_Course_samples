d21bf1f679f5d9d50aaddac7dc4a220faec4b444
Paata Home <paata.tepnadze@gmail.com>
2022-12-20 10:07:46 +0100

    create couter component

--- react_sam/src/App.js
+++ react_sam/src/App.js
@@ -1,23 +1,13 @@
-import { useState } from 'react';
+import Counter from './components/Counter';
 
 function App() {
-  const [like, setLike] = useState(0);
-  const [value, setValue] = useState('');
-
-  function increment() {
-    setLike(like + 1);
-  }
-  function decrement() {
-    setLike(like - 1);
-  }
-
   return (
     <div className="App">
-      <h1>{like} </h1>
-      <h1>{value} </h1>
-      <input type="inputs" onChange={(e) => setValue(e.target.value)} />
-      <button onClick={increment}>increment</button>
-      <button onClick={decrement}>decrement</button>
+      <Counter />
+
+      <Counter />
+      <Counter />
+      <Counter />
     </div>
   );
 }
