8ec2e852c660e757bc8ec39789800b59ed4b50c9
Paata Home <paata.tepnadze@gmail.com>
2022-12-20 14:28:20 +0100

    add select sorting by name/description

--- react_sam/src/App.js
+++ react_sam/src/App.js
@@ -2,6 +2,7 @@ import { useRef, useState } from 'react';
 import Postform from './components/Postform';
 
 import PostList from './components/PostList';
+import MySelect from './components/UI/select/MySelect';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -9,6 +10,8 @@ function App() {
     { id: 2, title: 'Javascript angular', body: 'descdiption' },
     { id: 4, title: 'Jav Vue js', body: 'descdiption' },
   ]);
+
+  const [selectedSort, setSelectedSort] = useState('');
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -18,9 +21,27 @@ function App() {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
 
+  const sortPost = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
+    console.log(sort);
+  };
+
   return (
     <div className="App">
       <Postform create={createPost} />
+      <hr style={{ margin: '25px 0' }} />
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPost}
+          defaultValue="sort"
+          options={[
+            { value: 'title', name: 'By name' },
+            { value: 'body', name: 'By description' },
+          ]}
+        />
+      </div>
       {posts.length !== 0 ? (
         <PostList
           remove={removePost}
